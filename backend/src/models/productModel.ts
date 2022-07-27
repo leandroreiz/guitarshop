d7e6215798f9862f8cba9382b0b1789c3a20ea51
Leandro Reis <leandro.reiz@gmail.com>
2022-07-27 19:18:18 +0100

    Add MongoDB interfaces/schemas/models

---
+++ backend/src/models/productModel.ts
@@ -0,0 +1,89 @@
+import mongoose from 'mongoose';
+
+interface IReview {
+  name: string;
+  rating: number;
+  comment: string;
+}
+
+interface IProduct {
+  user: mongoose.Schema.Types.ObjectId;
+  name: string;
+  image: string;
+  brand: string;
+  isAdmin: boolean;
+  category: string;
+  description: string;
+  reviews: Array<string>;
+  rating: number;
+  numReviews: number;
+  price: number;
+  countInStock: number;
+}
+
+const reviewSchema = new mongoose.Schema<IReview>(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const productSchema = new mongoose.Schema<IProduct>(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model<IProduct>('Product', productSchema);
+
+export default Product;
