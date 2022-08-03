63153700680a82e5c7a4fb5b020f69a4d5fdeab7
Leandro Reis <leandro.reiz@gmail.com>
2022-08-03 22:14:55 +0100

    Add update user profile functionality

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -89,3 +89,34 @@ export const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found!');
   }
 });
+
+/**
+ * Update user profile
+ * @route   PUT /api/v1/users/profile
+ * @access  Private
+ */
+export const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id.toString()),
+    });
+  } else {
+    res.status(404);
+    throw new Error('The update failed. Please try again!');
+  }
+});
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -4,6 +4,7 @@ import {
   getUsers,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from '../controllers/userController';
 import protect from '../middleware/authMiddleware';
 
@@ -11,6 +12,9 @@ const router = express.Router();
 
 router.route('/').get(getUsers).post(registerUser);
 router.route('/login').post(login);
-router.route('/profile').get(protect, getUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
